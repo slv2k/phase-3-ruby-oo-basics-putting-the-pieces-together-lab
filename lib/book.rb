2f6e8ae150812da9dc35ca1f2d4bc5c833f14a28
samuel <samuelmmanor@gmail.com>
2022-02-24 15:49:28 -0500

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author_name
+    attr_accessor :page_count
+    attr_accessor :genre
 
+    def initialize(title, author_name, page_count, genre)
+        @title = title
+        @author_name = author_name
+        @page_count =  page_count
+        @genre = genre
+    end
+end
 
