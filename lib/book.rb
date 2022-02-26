f325b2e8a1bd7701bac86d2c09a18806a3f531eb
samuel <samuelmmanor@gmail.com>
2022-02-26 17:08:26 -0500

    commit tree fa56fa735e49e3c221f6f8e2628dbfd3765165c6

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,12 @@
 class Book
-    attr_accessor :title
-    attr_accessor :author_name
-    attr_accessor :page_count
-    attr_accessor :genre
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
 
-    def initialize(title, author_name, page_count, genre)
-        @title = title
-        @author_name = author_name
-        @page_count =  page_count
-        @genre = genre
-    end
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
-
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Shoe
-    attr_accessor :color, :size, :material, :condition
-    attr_reader :brand
-  
-    def initialize(brand)
-      @brand = brand
-    end
-  
-    def cobble
-      @condition = "new"
-      puts "Your shoe is as good as new!"
-    end
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
   end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
